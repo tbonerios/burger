f5f6ef84f3a72e8f7bf6b63b111b1220473938aa
tbonerios <tbonerios@gmail.com>
2017-08-05 08:34:07 -0500

    updated mysql

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 ### Schema
 
 CREATE DATABASE burgers_db;
-USE burger_db;
+USE burgers_db;
 
 CREATE TABLE burgers
 (
@@ -11,3 +11,9 @@ CREATE TABLE burgers
 	date TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
+
+INSERT INTO burgers (burger_name) VALUES ('Old Fashioned Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hickory Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', true);
+
+SELECT * FROM burgers;
