f2c79a5feba98ad006af6a787f1bb192d21becbe
tbonerios <tbonerios@gmail.com>
2017-08-12 07:26:57 -0500

    more updating

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,11 +3,22 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
   burger.all(function(burger_data) {
     console.log(burger_data);
-    res.render("index", {burger_data});
+    res.render("index", {burger_data: burgerData});
   })
 })
+
+
+router.post("/burgers/create", function(req, res) {
+  burger.create(req.body.burger_name, function(result){
+    res.redirect("/");
+  });
+  });
 
 router.put("/burgers/update", function(req, res) {
   burger.update(req.body.burger_id, function(result){
@@ -15,12 +26,6 @@ router.put("/burgers/update", function(req, res) {
     res.redirect("/");
   });
 });
-
-router.post("/burgers/create", function(req, res) {
-  burger.create(req.body.burger_name, function(result){
-    res.redirect("/");
-  });
-  });
 
 
 
