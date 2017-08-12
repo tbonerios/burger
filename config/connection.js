3357685ed36448c07045f212a043753449002c4e
tbonerios <tbonerios@gmail.com>
2017-08-12 06:48:36 -0500

    another update

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
+
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 
-var connection = mysql.createConnection({
-  port: 3306,
+  connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Bubbacup50",
@@ -13,14 +14,7 @@ var connection = mysql.createConnection({
 });
 };
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
-// Export connection for our ORM to use.
+connection.connect()
+
 module.exports = connection;
