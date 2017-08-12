c4f83ca5e0713211b5392719605f9b3088dbfac9
tbonerios <tbonerios@gmail.com>
2017-08-12 07:57:19 -0500

    fix burgers_controllers.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,12 +2,16 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
-router.get("/", function(req, res) {
+router.get("/", function(req,res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
   burger.all(function(burger_data) {
     console.log(burger_data);
     res.render("index", {burger_data});
-  })
-})
+  });
+});
 
 router.put("/burgers/update", function(req, res) {
   burger.update(req.body.burger_id, function(result){
