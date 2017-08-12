43f76bb8c1e70fdf4d88db71a995db0d365c059f
tbonerios <tbonerios@gmail.com>
2017-08-12 07:35:49 -0500

    Heroku

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,22 +3,11 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-  res.redirect("/burgers");
-});
-
-router.get("/burgers", function(req, res) {
   burger.all(function(burger_data) {
     console.log(burger_data);
-    res.render("index", {burger_data: burgerData});
+    res.render("index", {burger_data});
   })
 })
-
-
-router.post("/burgers/create", function(req, res) {
-  burger.create(req.body.burger_name, function(result){
-    res.redirect("/");
-  });
-  });
 
 router.put("/burgers/update", function(req, res) {
   burger.update(req.body.burger_id, function(result){
@@ -26,6 +15,12 @@ router.put("/burgers/update", function(req, res) {
     res.redirect("/");
   });
 });
+
+router.post("/burgers/create", function(req, res) {
+  burger.create(req.body.burger_name, function(result){
+    res.redirect("/");
+  });
+  });
 
 
 
